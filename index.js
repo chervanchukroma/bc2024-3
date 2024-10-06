4a33a6066fd3819cd96b346e7271f20a9fe9fb80
chervanchukroma bc2024-3 <chervanchuckroma@gmail.com>
2024-10-06 19:02:31 +0300

    Implemented command line parameters and data processing

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 const fs = require('fs');
-const { program } = require('commander');
+const path = require('path');
+const { Command } = require('commander');
 
+const program = new Command();
+
+// Додайте аргументи командного рядка
 program
-  .requiredOption('-i, --input <path>', 'Input file path')  
-  .option('-o, --output <path>', 'Output file path')        
-  .option('-d, --display', 'Display output in console');    
+  .option('-i, --input <path>', 'Path to the input JSON file', '')
+  .option('-o, --output <path>', 'Path to the output file', '')
+  .option('-d, --display', 'Display results in console');
 
-program.parse(process.argv);  
+program.parse(process.argv);
 
-const options = program.opts();  
+// Отримайте значення аргументів
+const options = program.opts();
 
-if (!fs.existsSync(options.input)) {
-  console.error('Cannot find input file');
-  process.exit(1);  
+// Перевірка обов'язкового параметра
+if (!options.input) {
+  console.error('Please, specify input file');
+  process.exit(1);
 }
 
+// Читання даних з файлу
+let data;
 try {
-
-  const data = JSON.parse(fs.readFileSync(options.input, 'utf8'));
-
-  
-  const filteredData = data.filter(item => item.ku === 13 && item.value > 5);
-
-  
-  const result = filteredData.map(item => item.value).join('\n');
-
-  
-  if (options.output) {
-    fs.writeFileSync(options.output, result, 'utf8');
-  }
-
-  
-  if (options.display) {
-    console.log(result);
-  }
-
-  
+  const rawData = fs.readFileSync(path.resolve(options.input));
+  data = JSON.parse(rawData);
 } catch (error) {
-  console.error('Error reading or processing the file:', error);
+  console.error('Cannot find input file');
+  process.exit(1);
+}
+
+// Фільтрація даних
+const result = [];
+data.forEach(item => {
+  if (item.ku > 13 && item.value > 5) {
+    result.push(item.value);
+  }
+});
+
+// Вивід результатів
+if (options.display) {
+  console.log(result);
+}
+
+// Запис результатів у файл, якщо вказано
+if (options.output) {
+  fs.writeFileSync(path.resolve(options.output), JSON.stringify(result, null, 2));
 }
